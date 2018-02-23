2195893ba29737b767bbbec7074d24ab48eaf954
Fabien HIEGEL <fabien.hiegel@gmail.com>
2018-02-23 13:56:11 +0100

    Cards on Shelves | Card Editor | Create/Delete CardItem
    - Refactor : moves CardItemInputs in separate package

--- src/cards/CardEditor.js
+++ src/cards/CardEditor.js
@@ -4,6 +4,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 import { Card } from './Card.js';
+import { CardItemInput } from './inputs/CardItemInput.js';
 
 export class CardEditor extends React.Component {
     constructor(props) {
@@ -12,14 +13,16 @@ export class CardEditor extends React.Component {
             card: props.card
         };
         this.handleCardChange = this.handleCardChange.bind(this);
+        this.createItem = this.createItem.bind(this);
         this.updateItem = this.updateItem.bind(this);
+        this.removeItem = this.removeItem.bind(this);
     }
 
     render() {
         const card = this.state.card;
         const cardItems = card.content || [];
         const cardItemInputs = cardItems.map((item, index) =>
-            <CardItemInput key={index} item={item} onUpdate={ this.updateItem }/>
+            <CardItemEditor key={index} item={item} onUpdate={ this.updateItem } onRemove={ this.removeItem }/>
         );
 
         return (
@@ -28,13 +31,7 @@ export class CardEditor extends React.Component {
                 <Card card={card} />
                 <form className="card-editor">
                     <input name="name" type="text" value={card.name} onChange={ this.handleCardChange } />
-                    {false &&
-                    <div className="card-editor-toolbar">
-                        <button>Titre</button>
-                        <button>Zone de texte</button>
-                        <button>Clé/Valeur</button>
-                    </div>
-                    }
+                    <button type="button" onClick={this.createItem}>+</button>
                     <fieldset>
                         {cardItemInputs}
                     </fieldset>
@@ -54,6 +51,21 @@ export class CardEditor extends React.Component {
         this.updateCard(card);
     }
 
+    createItem() {
+        let card = this.state.card;
+        card.content.push({"type":"title", "content":"Nouveau"});
+
+        this.updateCard(card);
+    }
+    
+    removeItem(item) {
+        let card = this.state.card;
+        let itemIndex = card.content.indexOf(item);
+        card.content.splice(itemIndex, 1);
+
+        this.updateCard(card);
+    }
+
     updateCard(card) {
         this.setState({card: card});
     }
@@ -68,15 +80,27 @@ export class CardEditor extends React.Component {
     }
 }
 
-function CardItemInput(props){
-    let item = props.item;
-    if('title' == item.type) {
-        return <CardTitleInput item={item} onUpdate={props.onUpdate}/>;
+class CardItemEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: props.item
+        };
+        this.onUpdate = props.onUpdate;
+        this.onRemove = props.onRemove;
     }
-    if('field' == item.type) {
-        return <CardFieldInput item={item} onUpdate={props.onUpdate} />;
-    } 
-    return <CardTextInput item={item} onUpdate={props.onUpdate} />;
+
+    render() {
+        let item = this.state.item;
+        return (
+            <div className="card-item-editor">
+                <CardItemTypeSelector item={ item } onUpdate={ this.onUpdate } />
+                <CardItemInput item={ item } onUpdate={ this.onUpdate } />
+                <button type="button" onClick={ this.onRemove }>-</button>
+            </div>
+        );
+    }
+
 }
 
 class CardItemTypeSelector extends React.Component {
@@ -87,6 +111,25 @@ class CardItemTypeSelector extends React.Component {
         };
         this.handleTypeChange = this.handleTypeChange.bind(this);
         this.onUpdate = props.onUpdate;
+    }
+
+    render() {
+        let item = this.state.item;
+        const itemTypes = [
+            {"name": "title", "label": "Titre"},
+            {"name": "field", "label":"Clé/Valeur"},
+            {"name": "text", "label": "Texte long"}
+        ];
+        
+        const typesOptions = itemTypes.map((itemType, index) =>
+            <option key={index} value={itemType.name}>{itemType.label}</option>
+        );
+    
+        return (
+            <select value={item.type} onChange={ this.handleTypeChange }>
+                {typesOptions}
+            </select>
+        );
     }
 
     handleTypeChange(event) {
@@ -103,133 +146,4 @@ class CardItemTypeSelector extends React.Component {
         this.onUpdate(item);
     }
 
-    render() {
-        let item = this.state.item;
-        const options = ['title', 'field', 'text'];
-        
-        const typesOptions = options.map((type, index) =>
-                <option key={index} value={type}>{type}</option>
-            );
-    
-        return (
-            <select value={item.type} onChange={ this.handleTypeChange }>
-                {typesOptions}
-            </select>
-        );
-    }
-}
-class CardTitleInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            item: props.item
-        };
-        this.handleTitleChange = this.handleTitleChange.bind(this);
-        this.onUpdate = props.onUpdate;
-    }
-
-    render() {
-        const item = this.state.item;
-        return (
-            <div className="card-input-title">
-                <input type="text" value={item.content} onChange={ this.handleTitleChange } />
-                <CardItemTypeSelector item={item} onUpdate={ this.onUpdate }/>
-            </div>
-        );
-    }
-    
-    handleTitleChange(event) {
-        let item = this.state.item;
-        item.content = event.target.value;
-
-        this.updateItem(item)
-    }
-
-    updateItem(item){
-        this.setState({
-            item: item
-        });
-        this.onUpdate(item);
-    }
-}
-
-class CardFieldInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            item: props.item
-        };
-        this.handleHeaderChange = this.handleHeaderChange.bind(this);
-        this.handleValueChange = this.handleValueChange.bind(this);
-        this.onUpdate = props.onUpdate;
-    }
-
-    render() {
-        let item = this.state.item;
-        return (
-            <div className="card-input-field">
-                <input type="text" value={item.header} onChange={ this.handleHeaderChange } />
-                <input type="text" value={item.content} onChange={ this.handleValueChange } />
-                <CardItemTypeSelector item={item} onUpdate={ this.onUpdate }/>
-            </div>
-        );
-    }
-
-    handleHeaderChange(event) {
-        let item = this.state.item;
-        item.header = event.target.value;
-
-        this.updateItem(item)
-    }
-
-    handleValueChange(event) {
-        let item = this.state.item;
-        item.content = event.target.value;
-
-        this.updateItem(item)
-    }
-
-    updateItem(item){
-        this.setState({
-            item: item
-        });
-        this.onUpdate(item);
-    }
-
-}
-
-class CardTextInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            item: props.item
-        };
-        console.log('Text item type: ' + props.item.type);
-        this.handleTextChange = this.handleTextChange.bind(this);
-        this.onUpdate = props.onUpdate;
-    }
-
-    render() {
-        const item = this.state.item;
-        return (
-            <div className="card-input-text">
-                <textarea value={item.content} onChange={ this.handleTextChange } />
-                <CardItemTypeSelector item={item} onUpdate={ this.onUpdate }/>
-            </div>
-        );
-    }
-    
-    handleTextChange(event) {
-        let item = this.state.item;
-        item.content = event.target.value;
-
-        this.updateItem(item)
-    }
-
-    updateItem(item){
-        this.setState({
-            item: item
-        });
-        this.onUpdate(item);
-    }
 }
