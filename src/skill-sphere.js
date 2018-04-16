a5e0fcdb500da1f5473f740f058856ae5884c24a
Fabien HIEGEL <fabien.hiegel@gmail.com>
2018-04-16 13:17:50 +0200

    Skills-Spheres | Display as a readonly Codex

--- src/skill-sphere.js
+++ src/skill-sphere.js
@@ -2,11 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-import {Card} from './components/cards/Card.js';
 
 import './css/screen.css';
+import {Codex} from './components/codex/Codex';
 
 function aDiv(id) {
     const element = document.createElement('div');
@@ -17,29 +15,17 @@ function aDiv(id) {
 const skillSphereRoot = aDiv('skill-sphere');
 document.body.appendChild(skillSphereRoot);
 
-function SkillSphere(props) {
-    const cards = props.cards.map((card, index) =>
-        <Card key={index} card={card}/>
-    );
-    return (
-        <div id='spheres'>
-            <h1>Sphérier</h1>
-            {cards}
-        </div>
-    );
-}
-
-SkillSphere.propTypes = {
-    cards: PropTypes.array.isRequired
-};
-
 fetch('https://raw.githubusercontent.com/de-dale/skills-sphere/spheres.all/main/all.spheres')
-    .then(function(response) {
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
         ReactDOM.render(
-            <SkillSphere cards={data}/>,
+            <Codex
+                name="Sphérier"
+                cards={data}
+                editMode='readonly'
+            />,
             skillSphereRoot
         );
     });
