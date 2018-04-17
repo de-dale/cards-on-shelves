94e24a0d83574e3f2eaf7c8e7e864a6d70ef32f0
Fabien HIEGEL <fabien.hiegel@gmail.com>
2018-04-17 13:58:14 +0200

    Deck | Refactor | Simplify printing

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -15,13 +15,9 @@ export class Deck extends Component {
     }
 
     render() {
-        const cards = this.state.cards.map((item, index) => {
-            return (
-                <div key={index} className={((index + 1) % 9 === 0) ? 'page-break' : undefined}>
-                    <Card card={item}/>
-                </div>
-            );
-        });
+        const cards = this.state.cards.map((item, index) =>
+            <Card key={index} card={item}/>
+        );
         return (
             <div className="deck">
                 {cards}
