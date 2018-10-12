0fc3b1771fe8b3d872d51b6c19efc3401d6fa995
Fabien HIEGEL <fabien.hiegel@gmail.com>
2018-10-12 14:20:20 +0200

    refactor(redux): Fix index

--- src/index.js
+++ src/index.js
@@ -5,18 +5,9 @@ import store from './store';
 
 import Codex from './codex/Codex.js';
 
-import './css/screen.css';
 import { addCard } from './cards/list/actions';
 
-export function aDiv(id) {
-    const element = document.createElement('div');
-    element.setAttribute('id', id);
-    return element;
-}
-
-const codexRoot = aDiv('shelves-root');
-document.body.appendChild(codexRoot);
-
+const codexRoot = document.getElementById('shelves-root');
 loadCodex(codexRoot, 'Sphérier', 'https://raw.githubusercontent.com/de-dale/skills-sphere/spheres.all/main/all.spheres');
 
 function loadCodex(dom, name, url) {
@@ -36,10 +27,8 @@ function displayCodex(dom, name, data) {
         cards: data
     };
     ReactDOM.render(
-        <Provider store = {store}>
-            <Codex
-                codex={codex}
-            />
+        <Provider store={store}>
+            <Codex codex={codex}/>
         </Provider>,
         dom
     );
